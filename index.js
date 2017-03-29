45a3b054d6061f8c69937df34c23594f60aad47f
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-03-29 10:37:29 +0800

    22-封装选项卡指令实例（一）：绑定策略，绑定变量，与函数

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ var m1 = angular.module('app', []);
 m1.directive('myTab', function () {
     return {
         restrict:'E',
+        // scope:true,//独立作用域
+        // scope : {}, //隔离作用域，不受外部变量影响，配合内部contrller
+        scope : {   //作用域执行策略
+            myId : '@shangHua',  // @绑定的是字符串
+            myName : '=',    // =绑定的是数据，共享controller中的name数据，其实就是一个变量
+            myFn : '&' // &绑定函数
+        },
+        controller : ['$scope', function ($scope){
+            $scope.name = 'miaov2'
+        }],
         templateUrl : './tmp.html' 
     }
 });
 m1.controller('aaa', ['$scope', function ($scope) {
+    $scope.name = 'hello';
+    $scope.show = function (n) {
+        alert(n); 
+    }
 }]);
