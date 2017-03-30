446e43bbddd3e895fb0c7381fad7bfd549e13468
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-03-30 11:49:44 +0800

    23-封装选项卡指令实例（二）；添加jquery

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ var m1 = angular.module('app', []);
 m1.directive('myTab', function () {
     return {
         restrict:'E',
-        // scope:true,//独立作用域
+        // scope:true,  //独立作用域
         // scope : {}, //隔离作用域，不受外部变量影响，配合内部contrller
         scope : {   //作用域执行策略
-            myId : '@shangHua',  // @绑定的是字符串
-            myName : '=',    // =绑定的是数据，共享controller中的name数据，其实就是一个变量
-            myFn : '&' // &绑定函数
+            myId : '@',  // @绑定的是字符串
+            myData : '='
         },
         controller : ['$scope', function ($scope){
             $scope.name = 'miaov2'
         }],
+        link : function (scope, element, attr) {
+            // console.log(scope.name);
+            // console.log(attr.myId);
+            element.delegate('input', 'click', function () {
+                $(this).addClass('active').siblings('input').removeClass('active');
+                $(this).siblings('div').eq($(this).index()).show().siblings('div').hide();
+            });
+        },
         templateUrl : './tmp.html' 
     }
 });
 m1.controller('aaa', ['$scope', function ($scope) {
     $scope.name = 'hello';
-    $scope.show = function (n) {
-        alert(n); 
-    }
+    $scope.data1 = [
+        {title:'数学', content:'1+1=2'},
+        {title:'语文', content:'知之为知之，不知为不知'},
+        {title:'英语', content:'hello word'}
+    ];
+    $scope.data2 = [
+        {title:'物理', content:'万有引力定律'},
+        {title:'化学', content:'H2O'}
+    ]
 }]);
